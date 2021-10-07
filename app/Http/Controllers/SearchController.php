72c39f61d73c08073bcd18c5074e9199b046209b
Dave Schaatsbergen <2089667@talnet.nl>
2021-10-07 13:08:50 +0200

    sorting of the json data is done. made a beginning on pokemon page loadout

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -16,15 +16,291 @@ class SearchController extends Controller
             echo $e->getMessage();
             return view('Home');
         }
-        // gathering all pokemon data
+        // set all the variables
         $name = $pokemon_data['name'];
         $abilities = $pokemon_data['abilities'];
-        $moves = $pokemon_data['moves'];
         $dexNumber = $pokemon_data['id'];
+        $moves = $pokemon_data['moves'];
         $stats = $pokemon_data['stats'];
         $types = $pokemon_data['types'];
         $weight = $pokemon_data['weight'];
-        $sprite = $pokemon_data['sprites'];
-        return view('pokemon', compact('name', 'abilities', 'moves', 'dexNumber', 'stats', 'types', 'weight', 'sprite'));
+        $sprites = $pokemon_data['sprites']['versions']['generation-vii']['ultra-sun-ultra-moon'];
+        $icon = $pokemon_data['sprites']['versions']['generation-vii']['icons'];
+        // making arrays to order gen info in
+        $redBlue = [];
+        $yellow = [];
+        $goldSilver = [];
+        $crystal = [];
+        $rubySaphire = [];
+        $emerald = [];
+        $fireRedLeafGreen = [];
+        $diamondPearl = [];
+        $platinum = [];
+        $hearthgoldSoulsivler = [];
+        $blackWhite = [];
+        $blackWhite2 = [];
+        $xy = [];
+        $omegaAlpha = [];
+        $sunMoon = [];
+        $ultraSunMoon = [];
+        // looping through all the moves and order them in gen arrays
+        foreach ($moves as $move){
+            foreach ($move['version_group_details'] as $move_detail) {
+                switch($move_detail['version_group']['name']) {
+                    // pokemon red and blue
+                    case "red-blue":
+                        $redBlue = array_merge($redBlue, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                    // pokemon Yellow
+                    case "yellow":
+                        $yellow = array_merge($yellow, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                    // pokemon gold and silver
+                    case "gold-silver":
+                        $goldSilver = array_merge($goldSilver, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                    // pokemon crystal
+                    case "crystal":
+                        $crystal = array_merge($crystal, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                    // pokemon ruby and saphire
+                    case "ruby-sapphire":
+                        $rubySaphire = array_merge($rubySaphire, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                    // pokemon fire red and leaf green
+                    case "firered-leafgreen":
+                        $fireRedLeafGreen = array_merge($fireRedLeafGreen, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                    // pokemon emerald
+                    case "emerald":
+                        $emerald = array_merge($emerald, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                    //  pokemon diamond and pearl
+                    case "diamond-pearl":
+                        $diamondPearl = array_merge($diamondPearl, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                    // pokemon platinum
+                    case "platinum":
+                        $platinum = array_merge($platinum, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                    // pokemon hearthgold and soulsilver
+                    case "heartgold-soulsilver":
+                        $hearthgoldSoulsivler = array_merge($hearthgoldSoulsivler, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                    //  pokemon black and white
+                    case "black-white":
+                        $blackWhite = array_merge($blackWhite, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                    //  pokemon black and white 2
+                    case "black-2-white-2":
+                        $blackWhite2 = array_merge($blackWhite2, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                    // pokemon x and y
+                    case "x-y":
+                        $xy = array_merge($xy, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                    //  pokemon omega ruby and alpha saphire
+                    case "omega-ruby-alpha-sapphire":
+                        $omegaAlpha = array_merge($omegaAlpha, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                    // pokemon moon and sun
+                    case "sun-moon":
+                        $sunMoon = array_merge($sunMoon, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                    // pokemon ultra moon and sun
+                    case "ultra-sun-ultra-moon":
+                        $ultraSunMoon = array_merge($ultraSunMoon, array(array(
+                            "level_learned_at"=>$move_detail['level_learned_at'], 
+                            "move_name"=>$move['move']['name'], 
+                            "game"=> $move_detail['version_group']['name'], 
+                            "method"=> $move_detail['move_learn_method']['name'],
+                            "url" => $move['move']['url']
+                        )));
+                    break;
+                }
+            }
+        }
+        // usort all the arrays
+
+        // red blue
+        usort($redBlue, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // yellow
+        usort($yellow, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // gold silver
+        usort($goldSilver, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // crystal
+        usort($crystal, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // ruby saphire
+        usort($rubySaphire, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // emerald
+        usort($emerald, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // leafgreen firered
+        usort($fireRedLeafGreen, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // diamond pearl
+        usort($diamondPearl, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // platinum
+        usort($platinum, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // hearhgold soulsiver
+        usort($hearthgoldSoulsivler, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // black and white
+        usort($blackWhite, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // black and white 2
+        usort($blackWhite2, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // x & y
+        usort($xy, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // omega ruby alpha saphire
+        usort($omegaAlpha, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // sun moon 
+        usort($sunMoon, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // ultra sun moon
+        usort($ultraSunMoon, function ($item1, $item2) {
+            return $item1['level_learned_at'] <=> $item2['level_learned_at'];
+        });
+        // return the data to view
+        return view('pokemon', compact(
+            'name',
+            'abilities', 
+            'redBlue', 
+            'yellow', 
+            'goldSilver', 
+            'crystal', 
+            'rubySaphire', 
+            'emerald', 
+            'fireRedLeafGreen', 
+            'diamondPearl', 
+            'platinum', 
+            'hearthgoldSoulsivler', 
+            'blackWhite', 
+            'blackWhite2', 
+            'xy', 
+            'omegaAlpha', 
+            'sunMoon', 
+            'ultraSunMoon', 
+            'dexNumber', 
+            'stats', 
+            'types', 
+            'weight', 
+            'sprites',
+            'icon'));
     }
 }
--- resources/views/pokemon.blade.php
+++ resources/views/pokemon.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- jquery --}}
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <title
         >@section('title', 'pokemon '.$name.'')
     </title>
@@ -12,42 +14,65 @@
 <body>
     @section('content')
     <div class="pokemon_info_container">
-        {{-- stat container --}}
-        <div class="stat_container">
-            <table class="table table-striped table-dark">
-                <tr>
-                    <th class="base_stat_header" Colspan="6">Base Stats of {{$name}}</th>
-                </tr>
-                <tr>
-                @foreach ($stats as $base_stat)
-                    {!! '<th scope="col">' . $base_stat['stat']['name'] . '</th>' !!}
+        {{-- additional info --}}
+        <table class="table table-striped table-dark">
+            <tr>
+                <th class="base_stat_header devided4" Colspan="4">{{$name}} sprites</th>
+            </tr>
+            <tr>
+               <th scope="col" class="devided4">abilities</th>
+               <th scope="col" class="devided4">weight</th>
+               <th scope="col" class="devided4">National dex number</th>
+               <th scope="col" class="devided4">Name</th>
+            </tr>
+            <tr>
+                <td class="table_data ability">
+                @foreach ($abilities as $abilitiy)
+                    {!!"<p>" . $abilitiy['ability']['name']. "</p>"!!}
                 @endforeach
-                </tr>
-                <tr>
-                    @foreach ($stats as $base_stat)
-                    {!! '<td class="table_data">' . $base_stat['base_stat'] . '</td>' !!}
-                    @endforeach
-                </tr>
-            </table>
-        </div>
-         {{--info container  --}}
-        <div class="info_container">
-
-        </div> 
+                </td>
+                <td class="table_data ">{{$weight}} lbs</td>
+            </tr>
+        </table>
+        {{-- sprite container  --}}
+        <table class="table table-striped table-dark">
+            <tr>
+                <th class="base_stat_header devided4" Colspan="4">{{$name}} sprites</th>
+            </tr>
+            <tr>
+               <th scope="col" class="devided4">Normal</th>
+               <th scope="col" class="devided4">Shiny</th>
+               <th scope="col" class="devided4">Icon</th>
+               <th scope="col" class="devided4">Type(s)</th>
+            </tr>
+            <tr>
+                {!! '<td class="table_data"><img src="' . $sprites['front_default'] . '" alt="default front" class="spriteImage"></td>' !!}
+                {!! '<td class="table_data"><img src="' . $sprites['front_shiny'] . '" alt="shiny front" class="spriteImage"></td>' !!}
+                {!! '<td class="table_data"><img src="' . $icon['front_default'] . '" alt="icon front" class="spriteImage"></td>' !!}
+                @if (count($types) < 2)
+                    {!! '<td class="table_data"><img src="images/' . $types[0]['type']['name'] . '.png" alt="type 1"></td>' !!}
+                @else
+                    {!! '<td class="table_data types">
+                            <img src="images/' . $types[0]['type']['name'] . '.png" alt="type 1">
+                            <br>
+                            <img src="images/' . $types[1]['type']['name'] . '.png" alt="type 2">
+                        </td>' !!}
+                @endif
+            </tr>
+        </table>
         {{-- move_container --}}
-        <div class="move_container">
             <table class="table table-striped table-dark">
                 <tr>
-                    <th class="base_stat_header" Colspan="4">Move pool of {{$name}}</th>
+                    <th class="base_stat_header devided5" Colspan="5">Move pool of {{$name}}</th>
                 </tr>
                 <tr>
-                   <th scope="col">Level</th>
-                   <th scope="col">Name</th>
-                   <th scope="col">Game</th>
-                   <th scope="col">method</th>
-                   <th scope="col">Detail</th>
+                   <th scope="col" class="devided5">Level</th>
+                   <th scope="col" class="devided5">Name</th>
+                   <th scope="col" class="devided5">Game</th>
+                   <th scope="col" class="devided5">method</th>
+                   <th scope="col" class="devided5">Detail</th>
                 </tr>
-                @foreach ($moves as $move)
+                {{-- @foreach ($moves as $move)
                     <tr>
                         @foreach ($move['version_group_details'] as $move_detail)
                             
@@ -58,10 +83,40 @@
                             {!! '<td class="table_data"><a href="'.$move['move']['url'] . '">Detail</a></td>' !!}
                         @endforeach
                     </tr>
-                @endforeach
+                @endforeach --}}
             </table>
-        </div> 
+        {{-- base stats --}}
+        <table class="table table-striped table-dark">
+            <tr>
+                <th class="base_stat_header" Colspan="6">Base Stats of {{$name}}</th>
+            </tr>
+            <tr>
+            @foreach ($stats as $base_stat)
+                {!! '<th scope="col" class="devided6">' . $base_stat['stat']['name'] . '</th>' !!}
+            @endforeach
+            </tr>
+            <tr>
+                @foreach ($stats as $base_stat)
+                {!! '<td class="table_data devided6">' . $base_stat['base_stat'] . '</td>' !!}
+                @endforeach
+            </tr>
+        </table> 
     </div>  
     @endsection
+
+    {{-- pokeball loading animation --}}
+    <div class="loading_wrapper">
+        <div class="pokeball">
+            <div class="pokeball__button"></div>
+        </div>
+        Loading...
+    </div>
+    <script>
+        jQuery(document).ready(function($) {
+            $(window).on("load",function(){
+                $(".loading_wrapper").fadeOut( "slow" );
+            });
+        });
+    </script>
 </body>
 </html>
